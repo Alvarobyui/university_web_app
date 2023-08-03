4c47e328674845f40041f98c3f70c2678d496817
Alvarobyui <u701860138@byui.edu>
2023-08-03 00:13:22 -0500

    side bar implemented

--- view/admin_views/dashboard.php
+++ view/admin_views/dashboard.php
@@ -9,41 +9,60 @@
 </head>
 
 <body class="h-[100vh]">
-  <div class="text-2xl flex h-[100%]">
-    <aside class="p-4 w-56 text-sm bg-[#3f454c] static text-neutral-300">
-      <div class="logo flex gap-2 items-center">
+  <div class="text-2xl flex h-full">
+    <aside class=" w-[235px] min-w-[235px] text-sm bg-[#3f454c] static text-neutral-300">
+      <div class="logo p-4 flex gap-2 items-center border-b-[1px] border-solid border-blue-100">
         <div class="img rounded-full grid place-items-center w-[35px] h-[35px]"><img class="rounded-full h-full w-full" src="../../assets/logo-small.png" alt="uni"></div>
-        <h2>Universidad</h2>
+        <h2 class="text-xl">Universidad</h2>
       </div>
-      <div class="rol">
+      <div class="rol p-4 border-b-[1px] border-solid border-blue-100">
         <p>Alvaro Diaz</p>
         <p>Administrador</p>
       </div>
-      <div class="menu">
-        <p>MENU DE ADMINISTRACIÓN</p>
+      <div class="menu p-4">
+        <p class="pb-4 pl-6">MENU ADMINISTRACIÓN</p>
         <nav>
           <ul>
             <li>
-              <a href="permisos.php">
-                <div class="img"><img src="permisos.png" alt="ico"></div>
+              <a href="permisos.php" class="flex gap-2 items-center pb-2 hover:">
+                <div class="img">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-lock" viewBox="0 0 16 16">
+                    <path d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm0 5.996V14H3s-1 0-1-1 1-4 6-4c.564 0 1.077.038 1.544.107a4.524 4.524 0 0 0-.803.918A10.46 10.46 0 0 0 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h5ZM9 13a1 1 0 0 1 1-1v-1a2 2 0 1 1 4 0v1a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1v-2Zm3-3a1 1 0 0 0-1 1v1h2v-1a1 1 0 0 0-1-1Z" />
+                  </svg>
+                </div>
                 <p>Permisos</p>
               </a>
             </li>
             <li>
-              <a href="maestros.php">
-                <div class="img"><img src="maestros.png" alt="ico"></div>
+              <a href="maestros.php" class="flex gap-2 items-center pb-2 hover:">
+                <div class="img">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-video3" viewBox="0 0 16 16">
+                    <path d="M14 9.5a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm-6 5.7c0 .8.8.8.8.8h6.4s.8 0 .8-.8-.8-3.2-4-3.2-4 2.4-4 3.2Z" />
+                    <path d="M2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h5.243c.122-.326.295-.668.526-1H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v7.81c.353.23.656.496.91.783.059-.187.09-.386.09-.593V4a2 2 0 0 0-2-2H2Z" />
+                  </svg>
+                </div>
                 <p>Maestros</p>
               </a>
             </li>
             <li>
-              <a href="Alumnos.php">
-                <div class="img"><img src="alumnos.png" alt="ico"></div>
+              <a href="Alumnos.php" class="flex gap-2 items-center pb-2 hover:">
+                <div class="img">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mortarboard-fill" viewBox="0 0 16 16">
+                    <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917l-7.5-3.5Z" />
+                    <path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466 4.176 9.032Z" />
+                  </svg>
+                </div>
                 <p>Alumnos</p>
               </a>
             </li>
             <li>
-              <a href="clases.php">
-                <div class="img"><img src="clases.png" alt="ico"></div>
+              <a href="clases.php" class="flex gap-2 items-center pb-2 hover:">
+                <div class="img">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-workspace" viewBox="0 0 16 16">
+                    <path d="M4 16s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H4Zm4-5.95a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
+                    <path d="M2 1a2 2 0 0 0-2 2v9.5A1.5 1.5 0 0 0 1.5 14h.653a5.373 5.373 0 0 1 1.066-2H1V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v9h-2.219c.554.654.89 1.373 1.066 2h.653a1.5 1.5 0 0 0 1.5-1.5V3a2 2 0 0 0-2-2H2Z" />
+                  </svg>
+                </div>
                 <p>Clases</p>
               </a>
             </li>
